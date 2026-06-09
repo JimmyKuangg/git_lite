e7ff879ed82d6306d50b91338b3d9e7e4cdc5aad
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-06-09 14:32:16 -0700

    Adds tree encoding function

--- tree.go
+++ tree.go
@@ -1,6 +1,10 @@
 package core
 
-import "errors"
+import (
+	"bytes"
+	"errors"
+	"sort"
+)
 
 type TreeEntry struct {
   Name string
@@ -37,4 +41,23 @@ func (t *Tree) Add(name string, typeof string, hash string) error {
 	})
 
 	return nil
+}
+
+func (t *Tree) Encode() []byte {
+	sort.Slice(t.Entries, func(i, j int) bool {
+		return t.Entries[i].Name < t.Entries[j].Name
+	})
+
+	var buf bytes.Buffer
+
+	for _, entry := range t.Entries {
+		buf.WriteString(entry.Type)
+		buf.WriteByte(' ')
+		buf.WriteString(entry.Name)
+		buf.WriteByte(' ')
+		buf.WriteString(entry.Hash)
+		buf.WriteByte('\n')
+	}
+
+	return buf.Bytes()
 }
