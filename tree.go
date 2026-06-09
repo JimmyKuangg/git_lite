7d894582ace8e7ca07447d229a9b7dbfce296620
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-06-09 14:08:42 -0700

    Adds tree add method

---
+++ tree.go
@@ -0,0 +1,35 @@
+package core
+
+import "errors"
+
+type TreeEntry struct {
+  Name string
+  Type string
+  Hash string
+}
+
+type Tree struct {
+  Entries []TreeEntry
+}
+
+func (t *Tree) Add(name string, typeof string, hash string) error {
+	if (name == "") {
+		return errors.New("invalid name")
+	}
+
+	if (typeof != "blob" && typeof != "tree") {
+		return errors.New("invalid type")
+	}
+
+	if (hash == "") {
+		return errors.New("invalid hash")
+	}
+
+	t.Entries = append(t.Entries, TreeEntry{
+		Name: name,
+		Type: typeof,
+		Hash: hash,
+	})
+
+	return nil
+}
