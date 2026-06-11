0d311ce4bfe87ce4dd3dee89789c5362a36b3e60
Jimmy Kuang <66882632+JimmyKuangg@users.noreply.github.com>
2026-06-11 13:45:44 -0700

    Adds Read and Write for head (#5)

---
+++ head.go
@@ -0,0 +1,30 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func WriteHEAD(hash string) error {
+	if hash == "" {
+		return errors.New("cannot write empty HEAD")
+	}
+
+	err := os.WriteFile(".gitlite/HEAD", []byte(hash), 0644)
+	if err != nil {
+		return fmt.Errorf("failed to write HEAD: %w", err)
+	}
+
+	return nil
+}
+
+func ReadHEAD() (string, error) {
+	data, err := os.ReadFile(".gitlite/HEAD")
+	if err != nil {
+		return "", fmt.Errorf("failed to read HEAD: %w", err)
+	}
+
+	return strings.TrimSpace(string(data)), nil
+}
