0cb24269b058e17030d8997362d49f0df246705a
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-06-11 12:06:21 -0700

    Adds commit encoding function

--- commit.go
+++ commit.go
@@ -1,8 +1,41 @@
 package core
 
+import (
+	"bytes"
+	"time"
+)
+
 type Commit struct {
-	Root string
-	Author string
-	Message string
-	Parent string
+	Timestamp time.Time
+	Author 		string
+	Message 	string
+	Root 			string
+	Parent 		string
+}
+
+func (c *Commit) Encode() []byte {
+	var buf bytes.Buffer
+
+	buf.WriteString("tree ")
+	buf.WriteString(c.Root)
+	buf.WriteByte('\n')
+
+	if c.Parent != "" {
+			buf.WriteString("parent ")
+			buf.WriteString(c.Parent)
+			buf.WriteByte('\n')
+	}
+
+	buf.WriteString("author ")
+	buf.WriteString(c.Author)
+	buf.WriteByte('\n')
+
+	buf.WriteString("timestamp ")
+	buf.WriteString(c.Timestamp.UTC().Format(time.RFC3339))
+	buf.WriteByte('\n')
+
+	buf.WriteByte('\n')
+
+	buf.WriteString(c.Message)
+	return buf.Bytes()
 }
