3d59287fcbe941611a25a8eafda9391574f94d00
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-06-15 19:06:07 -0700

    Adds ReadIndex func

--- core/index.go
+++ core/index.go
@@ -1,5 +1,11 @@
 package core
 
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
 type IndexEntry struct {
 	Path string
 	Hash string
@@ -9,6 +15,39 @@ type Index struct {
 	Entries map[string]string
 }
 
+const IndexPath = ".gitlite/index"
+
 func (i *Index) Add(path string, hash string) {
 	i.Entries[path] = hash
+}
+
+func ReadIndex() (*Index, error) {
+	content, err := os.ReadFile(IndexPath)
+  if os.IsNotExist(err) {
+    return &Index{
+        Entries: make(map[string]string),
+    }, nil
+	} else if err != nil {
+		return nil, fmt.Errorf("unable to read index: %w", err)
+	}
+
+	index := &Index{
+    Entries: make(map[string]string),
+	}
+	lines := strings.Split(string(content), "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+
+		split := strings.Split(line, " ")
+		if len(split) != 2 {
+			return nil, fmt.Errorf("invalid index entry: %s", line)
+		}
+		
+		index.Entries[split[0]] = split[1]
+	}
+
+	return index, nil
 }
