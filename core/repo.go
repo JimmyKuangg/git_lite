3544b711bc161012bdc76fab6db3e67caa76b8d5
Jimmy Kuang <jimmy.kuang96@gmail.com>
2026-06-23 14:25:55 -0700

    Adds repo root check function

---
+++ core/repo.go
@@ -0,0 +1,36 @@
+package core
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+func EnsureGitliteRepo() (string, error) {
+	current, err := os.Getwd()
+	if err != nil {
+    return "", fmt.Errorf("cannot get working directory: %w", err)
+	}
+
+	for {
+    gitDir := filepath.Join(current, ".gitlite")
+		info, err := os.Stat(gitDir);
+
+    if err == nil && info.IsDir() {
+      return current, nil
+    }
+		
+		if err != nil && !os.IsNotExist(err) {
+			return "", fmt.Errorf("error checking directory: %w", err)
+		}
+
+    parent := filepath.Dir(current)
+
+    if parent == current {
+      return "", errors.New("not a gitlite repository")
+    }
+
+    current = parent
+	}
+}
